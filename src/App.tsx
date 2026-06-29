b95d3a80e92a9479d411226f25d02a3f37e5bf55
dionysia-lemonaki <105449958+dionysia-lemonaki@users.noreply.github.com>
2026-06-29 15:46:29 +0300

    style: add App component styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import data from "./data";
 
 const App = () => {
   return (
-    <main>
-      <div>
-        <div>
+    <main className="min-h-screen flex flex-col justify-center items-center p-6">
+      <div className="bg-white max-w-[37.5rem] flex flex-col gap-6 md:gap-8 p-6 md:p-10 rounded-2xl">
+        <div className="flex gap-6 items-center">
           <img src={iconStar} alt="" width="40" height="40" />
-          <h2>FAQs</h2>
+          <h2 className="text-[2rem] md:text-[3.5rem] md:leading-[1.178] font-bold text-(--purple-950)">
+            FAQs
+          </h2>
         </div>
         <Accordion data={data} />
       </div>
